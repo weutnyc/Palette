704477437f4e5ef70b55511ff8c8909510d65bdc
weutnyc <weutnyc@gmail.com>
2021-05-21 14:16:52 +0300

    norm

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var paletteView: UIView!
+    
     @IBOutlet var redLable: UILabel!
     @IBOutlet var greenLable: UILabel!
     @IBOutlet var blueLable: UILabel!
@@ -21,26 +22,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         paletteView.layer.cornerRadius = 10
         
         redLable.text = String(redSlider.value)
         greenLable.text = String(greenSlider.value)
         blueLable.text = String(blueSlider.value)
-        
     }
     
     override func viewWillLayoutSubviews() {
-            paletteView.backgroundColor = UIColor(
-                displayP3Red: CGFloat(redSlider.value),
-                green: CGFloat(greenSlider.value),
-                blue: CGFloat(blueSlider.value),
-                alpha: 1.0
-            )
-        }
+        paletteView.backgroundColor = UIColor(
+            displayP3Red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
+    }
     
     @IBAction func redSliderAction() {
         redLable.text = String(format: "%.2f", redSlider.value)
-        
     }
     
     @IBAction func greenSliderAction() {
